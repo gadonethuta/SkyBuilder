28cb9af63f014ef40aa28f4a1daafa6e9f67c5b9
WEI SHIUAN CHANG <simba@daredevil.pro>
2024-10-15 16:10:29 +0900

    Jump

---
+++ Value.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+/// <summary>
+/// Max値がある変数
+/// </summary>
+struct ValueInt {
+	int now;
+	int max;
+};
+
+/// <summary>
+/// Max値がある変数
+/// </summary>
+struct ValueFloat {
+	float now;
+	float max;
+};
--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include <Novice.h>
 #include <Vector2.h>
 #include "BasicFunctions.h"
-#include <Block.h>
+#include "Block.h"
+#include "Player.h"
 
 const char kWindowTitle[] = "GC1B_02_イグチ_GC1B_09_チョウイセン_GC1A_07_テュウタ_スカイビルダー";
 
 float rightBorder = 528.0f;
 
-Vector2 playerPos = { 200.0f, 100.0f };
-Vector2 playerScreenPos = { 200.0f, 100.0f };
+//Vector2 playerPos = { 200.0f, 100.0f };
+//Vector2 playerScreenPos = { 200.0f, 100.0f };
 
 
 // Windowsアプリでのエントリーポイント(main関数)
@@ -19,7 +20,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	int mockupGraph = Novice::LoadTexture("./Resources/Sprites/mockup1.png");
 	int blockGraph = Novice::LoadTexture("./Resources/Sprites/basicBlock.png");
-
+	int pointerGraph = Novice::LoadTexture("./Resources/Sprites/pointer.png");
+	int jumpLevelGraph = Novice::LoadTexture("./Resources/Sprites/jumpLevel.png");
 	
 
 	// キー入力結果を受け取る箱
@@ -50,6 +52,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		}
 
 		if (keys[DIK_C] && !preKeys[DIK_C]) {
+			PlayerInitialization(player, { 250,0 });
 			for (int i = 0; i < kBlockNum; i++) {
 				if (!block[i].isVisible) {
 					BlockCreate(block[i], { 0,700 }, { 5, 15 });
@@ -58,17 +61,23 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			}
 		}
 
+		
+
 #pragma endregion
 
 #pragma region update
-		
 		BlockUpdate();
+		PlayerUpdate(player);
+		
+		if (keys[DIK_SPACE] && !preKeys[DIK_SPACE]) {
+			Jump(player, 35.0f);
+		}
 
 #pragma endregion
 
 #pragma region convert2Screen
 
-		playerScreenPos = World2Screen(playerPos, worldOrigin);
+		player.screenPos = World2Screen(player.pos, worldOrigin);
 		for (int i = 0; i < kBlockNum; i++) {
 			block[i].screenPos = World2Screen(block[i].pos, worldOrigin);
 		}
@@ -86,7 +95,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		Novice::DrawEllipse(static_cast<int>(worldOrigin.x), static_cast<int>(worldOrigin.y), 10, 10, 0.0f, RED, kFillModeSolid);
 		
 		
-		Novice::DrawEllipse(static_cast<int>(playerScreenPos.x), static_cast<int>(playerScreenPos.y), 10, 10, 0.0f, BLUE, kFillModeSolid);
+		Novice::DrawEllipse(static_cast<int>(player.screenPos.x), static_cast<int>(player.screenPos.y), 100, 100, 0.0f, BLACK, kFillModeSolid);
+
+		Novice::ScreenPrintf(0, 0, "PlayerPos : %.2f, %.2f", player.screenPos.x, player.screenPos.y);
+		Novice::ScreenPrintf(0, 20, "jumpPower: %.2f", jumpPower);
+		Novice::ScreenPrintf(0, 40, "gauge: %d", gauge);
 
 #pragma endregion
 
